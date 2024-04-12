2b97811384d566e16bd6bcc1e4732f6b34464362
Нисанов Даня <emildaniil@gmail.com>
2024-04-13 01:08:39 +0300

    Ready

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Нисанов Даниил, РИБО-04-22, Вариант 2");
         Thread[] students = new StudentThread[7];
         for (int i = 0; i < 7; i++) {
             students[i] = new StudentThread();
